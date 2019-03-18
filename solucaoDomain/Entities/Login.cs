80110adaff303485dd447885dfe4cc2f82bc7225
Valnei Vinicios Santos de Sousa <vinnyonix@gmail.com>
2019-03-18 14:25:23 -0300

    Login
    AdicionadoClasse Login

---
+++ solucaoDomain/Entities/Login.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solucaoDomain.Entities
+{
+    public class Login
+    {
+        public int IDLogin { get; set; }
+        public string Email { get; set; }
+        public DateTime DataUltimoAcesso { get { return this.DataUltimoAcesso; } set { value = DateTime.Now; } }        
+    }
+}
